9536246c7174cec6aea792fdc18b73abc5a61211
kimjinu <kimjinu5555@gmail.com>
2022-10-11 17:13:16 +0900

    [Implement] ImageMeta 스키마 정의

---
+++ Image Hiscord/Source/Model/ImageMeta.swift
@@ -0,0 +1,14 @@
+//
+//  ImageMeta.swift
+//  Image Hiscord
+//
+//  Created by 김진우 on 2022/10/11.
+//
+
+struct ImageMeta {
+    let name: String
+    let date: String
+    let time: String
+    let latitude: String
+    let longitude: String
+}
