eb9ee55b70e19eed9a121f1c68821f2795fe73d3
kimjinu <kimjinu5555@gmail.com>
2022-12-25 19:03:18 +0900

    Add HashService

---
+++ Image Hiscord/Source/Service/HashService.swift
@@ -0,0 +1,16 @@
+//
+//  HashService.swift
+//  Image Hiscord
+//
+//  Created by 김진우 on 2022/12/25.
+//
+
+final class HashService {
+    func encode(salt: String, content: String) -> String {
+        return content
+    }
+    
+    func decode(salt: String, content: String) -> String {
+        return content
+    }
+}
